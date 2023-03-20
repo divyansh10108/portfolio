fc9787b45342669897be67b350b3d6b42e2fe47d
SilmeyButter <95627241+divyansh10108@users.noreply.github.com>
2023-03-20 07:22:06 +0530

    please work2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           © 2023 Divyansh Gaba<a href="/" className="hover:underline"></a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-          <a href="https://github.com/divyansh10108" rel="" target="_blank">
+          <a href="https://github.com/divyansh10108" rel="noreferrer" target="_blank">
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
@@ -22,8 +22,8 @@ const Footer = () => {
           </a>
           <a
             href="https://twitter.com/slimey_butter"
-            rel=""
-            target=""
+            rel="noreferrer"
+            target="_blank"
           >
             <AiOutlineTwitter
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
